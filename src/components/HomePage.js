0013f5d5cdad82c304e2b185013144582d66df01
Nam <namdt@NamMacbook.local>
2023-07-03 14:26:00 +0700

    commit 1

---
+++ src/components/HomePage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function HomePage(props) {
+  return (
+    <div
+      style={{
+        color: "rebeccapurple",
+      }}
+    >
+      Home page
+    </div>
+  );
+}
+
+export default HomePage;
