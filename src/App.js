54b2321d3e8e49e61ca36bf957743324bea66172
kiainamdt <nam.dt247@gmail.com>
2023-07-03 15:04:47 +0700

    rebase06

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World Rebase Main 1
+          Hello World Rebase Main 02-child
         </p>
         <HomePage />
 
